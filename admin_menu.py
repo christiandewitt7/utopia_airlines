12cb312b32216ff28f76ba523aa667148aabd8ba
cxdew <christian.dewitt@smoothstack.com>
2021-12-08 18:13:32 -0500

    added airport_options_menu

--- admin_menu.py
+++ admin_menu.py
@@ -9,9 +9,20 @@ def a_menu():
     print("7) Over-ride Trip Cancellation for a ticket.")
     user_response = input_from_user()
     print(user_response)
+    if user_response == 4:
+        airport_options_menu()
+
 
 def input_from_user():
     print("Select number: ")
     answer = int(input())
     return answer
 
+def airport_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    airports_options_response = input_from_user()
+    print(airports_options_response)
