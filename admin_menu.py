0d46985d36716b732547c52543b759672b2ceb30
cxdew <christian.dewitt@smoothstack.com>
2021-12-09 01:16:59 -0500

    added all options menu

--- admin_menu.py
+++ admin_menu.py
@@ -1,3 +1,6 @@
+import all_options_menu
+
+
 def a_menu():
     print("Please select your admin function:\n")
     print("1) Add / Update / Delete / Read: Flights")
@@ -9,8 +12,22 @@ def a_menu():
     print("7) Over-ride Trip Cancellation for a ticket.")
     user_response = input_from_user()
     print(user_response)
+    if user_response == 1:
+        all_options_menu.flights_options_menu()
+    if user_response == 2:
+        all_options_menu.seats_options_menu()
+    if user_response == 3:
+        all_options_menu.tickets_and_passengers_options_menu()
     if user_response == 4:
-        airport_options_menu()
+        all_options_menu.airports_options_menu()
+    if user_response == 5:
+        all_options_menu.travelers_options_menu()
+    if user_response == 6:
+        all_options_menu.employees_options_menu()
+    if user_response == 7:
+        all_options_menu.override_options_menu()
+
+
 
 
 def input_from_user():
@@ -18,7 +35,7 @@ def input_from_user():
     answer = int(input())
     return answer
 
-def airport_options_menu():
+def airports_options_menu():
     print("Please select from one of the four options:\n")
     print("1) Add")
     print("2) Update")
@@ -26,3 +43,4 @@ def airport_options_menu():
     print("4) Read")
     airports_options_response = input_from_user()
     print(airports_options_response)
+
---
+++ all_options_menu.py
@@ -0,0 +1,97 @@
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def flights_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    flights_options_response = input_from_user()
+    print(flights_options_response)
+
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def seats_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    seats_options_response = input_from_user()
+    print(seats_options_response)
+
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def tickets_and_passengers_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    tickets_and_passengers_options_response = input_from_user()
+    print(tickets_and_passengers_options_response)
+
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def airports_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    airports_options_response = input_from_user()
+    print(airports_options_response)
+
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def travelers_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    travelers_options_response = input_from_user()
+    print(travelers_options_response)
+
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def employees_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    employees_options_response = input_from_user()
+    print(employees_options_response)
+
+def input_from_user():
+    print("Select number: ")
+    answer = int(input())
+    return answer
+
+def override_options_menu():
+    print("Please select from one of the four options:\n")
+    print("1) Add")
+    print("2) Update")
+    print("3) Delete")
+    print("4) Read")
+    override_options_response = input_from_user()
+    print(override_options_response)
